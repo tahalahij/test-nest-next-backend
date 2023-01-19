39944d80b3558da6102543242ad04cc0345c29ff
taha <tahalahij@gmail.com>
2023-01-19 13:50:35 +0330

    feat: add MongooseModule config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionService } from './transaction/transaction.service';
+import { UserModule } from './user/user.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { GameModule } from './game/game.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    TransactionModule,
+    GameModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+      expandVariables: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      connectionName: 'firstDB',
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get('app.firstDB'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TransactionService],
 })
 export class AppModule {}
