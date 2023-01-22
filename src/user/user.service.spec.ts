8425179415aa404385429c098b3637f3c379e728
taha <tahalahij@gmail.com>
2023-01-22 22:32:27 +0330

    enhance: remove redundant tests (e2e tests )

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,22 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { getModelToken } from '@nestjs/mongoose';
-import { User, UserDocument } from './user.schema';
-import mongoose, { Model } from 'mongoose';
+import { User } from './user.schema';
+import mongoose from 'mongoose';
 import { CryptoService } from './crypto.service';
+import { ConfigModule } from '@nestjs/config';
 
+const mockUserModel = () => ({});
 describe('UserService', () => {
   let service: UserService;
   let cryptoService: CryptoService;
-  let userModel: Model<UserDocument>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: ['.env'],
+          expandVariables: true,
+        }),
+      ],
       providers: [
         UserService,
         {
           provide: getModelToken(User.name),
-          useFactory: User,
+          useFactory: mockUserModel,
         },
         CryptoService,
       ],
@@ -24,7 +32,6 @@ describe('UserService', () => {
 
     service = module.get<UserService>(UserService);
     cryptoService = module.get<CryptoService>(CryptoService);
-    userModel = module.get<Model<User>>(getModelToken(User.name));
   });
 
   it('should be defined', () => {
@@ -32,15 +39,16 @@ describe('UserService', () => {
   });
 
   it('should  validate User', async () => {
-    const user = await userModel.create({
+    mockUserModel['findOne'] = jest.fn().mockResolvedValue({
       _id: new mongoose.Types.ObjectId('63cd1f5e9c9abe5feca3fad6'),
       name: 'user 1',
       email: 'user1@gmail.com',
       password: await cryptoService.hashPassword('123'),
       createdAt: new Date(),
     });
+
     const { name, id } = await service.validateUser({ password: '123', email: 'user1@gmail.com' });
-    expect(user).toHaveProperty('name', name);
-    expect(user).toHaveProperty('_id', id);
+    expect(name).toBe('user 1');
+    expect(id).toBe('63cd1f5e9c9abe5feca3fad6');
   });
 });
